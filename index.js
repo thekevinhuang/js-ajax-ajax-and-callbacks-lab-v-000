45e0780b669d2083a141ee4f08c37e2412e78c26
Kevin Huang <yellow.khaos@gmail.com>
2018-11-14 22:29:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var renderCommit = (commit) => {
   return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
 }
 
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
 var showCommits = (el) => {
   $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
     $('#details').html(renderCommits(data))
